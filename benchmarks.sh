6f75ad9834656c72c6731074c8a81c0ee093d2f9
Whiteknight <wknight8111@gmail.com>
2010-02-06 22:59:32 -0500

    a quick sh script to run the benchmarks

---
+++ benchmarks.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Queue benchmarks:"
+for i in benchmarks/*queue.pir; do
+    parrot $i
+done
+
+echo "Stack benchmarks:"
+for i in benchmarks/*stack*.pir; do
+    parrot $i
+done
+
+echo "Array benchmarks:"
+for i in benchmarks/*array.pir; do
+    parrot $i
+done
+
